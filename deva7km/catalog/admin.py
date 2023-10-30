d0c39bb14a4f14299319270f95be5cdaafc4354f
zharuk <deva7km@gmail.com>
2023-10-30 22:04:31 +0200

    навел порядок

--- deva7km/catalog/admin.py
+++ deva7km/catalog/admin.py
@@ -1,98 +1,48 @@
 from django.contrib import admin
-from django.utils.html import format_html
 from .models import Product, ProductModification, Category, Image, Color, Size
 
 
+# Создаем класс ImageAdmin, настраивающий отображение модели Image в административной панели.
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('modification', 'thumbnail_image')
-    list_display_links = ('modification', 'thumbnail_image')
-
-    def thumbnail_image(self, obj):
-        return format_html('<img src="{}"/>', obj.thumbnail.url)
-
-    thumbnail_image.allow_tags = True
-    thumbnail_image.short_description = 'Миниатюра изображения'
 
 
+# Создаем класс ImageInline для встраивания модели Image в административную панель ProductModification.
 class ImageInline(admin.StackedInline):
     model = Image
     extra = 1  # Количество пустых форм для добавления изображений
     fields = [('image', 'thumbnail_image')]  # Добавляем миниатюру в список отображаемых полей
     readonly_fields = ('thumbnail_image',)
 
-    def thumbnail_image(self, obj):
-        return format_html('<img src="{}"/>', obj.thumbnail.url)
 
-    thumbnail_image.allow_tags = True
-    thumbnail_image.short_description = 'Миниатюра изображения'
-
-
+# Создаем класс ProductModificationAdmin для настройки отображения модели ProductModification в административной панели.
 class ProductModificationAdmin(admin.ModelAdmin):
-    list_display = ('product', 'custom_sku', 'color', 'size', 'stock', 'price', 'currency', 'thumbnail_image',)
+    list_display = ('product', 'custom_sku', 'color', 'size', 'stock', 'price', 'currency', 'thumbnail_image_modification',)
     list_filter = ('color', 'size', 'custom_sku',)
     search_fields = ('product__title', 'color__name', 'size__name', 'custom_sku')
-    inlines = [ImageInline]
-
-    def thumbnail_image(self, obj):
-        images = Image.objects.filter(modification=obj)  # Получаем изображения, связанные с данной модификацией
-        if images:
-            return format_html('<img src="{}"/>', images[0].thumbnail.url)
-        return format_html('<p>No Image</p>')
-
-    thumbnail_image.allow_tags = True
-    thumbnail_image.short_description = 'Миниатюра изображения'
+    inlines = [ImageInline]  # Встраиваем ImageInline для отображения изображений внутри ProductModification.
 
 
+# Создаем класс ProductModificationInline для встраивания модели ProductModification в административную панель Product.
 class ProductModificationInline(admin.TabularInline):
     model = ProductModification
     extra = 0  # Количество пустых форм для добавления модификаций
-    list_display = ('product', 'custom_sku', 'color', 'size', 'stock', 'price', 'currency')
-    readonly_fields = ('get_thumbnail_image',)  # Добавляем метод get_thumbnail_image в readonly_fields
-
-    def get_thumbnail_image(self, obj):
-        images = Image.objects.filter(modification=obj)  # Получаем изображения, связанные с данной модификацией
-        # print(images)
-        if images:
-            return format_html('<img src="{}"/>', images[0].thumbnail.url)
-        return format_html('<p>No Image</p>')
-
-    get_thumbnail_image.allow_tags = True
-    get_thumbnail_image.short_description = 'Миниатюра изображения'
+    list_display = ('product', 'custom_sku', 'color', 'size', 'stock', 'price', 'currency',)
+    readonly_fields = ('thumbnail_image_modification',)
 
 
+# Создаем класс ProductAdmin для настройки отображения модели Product в административной панели.
 class ProductAdmin(admin.ModelAdmin):
-    inlines = [ProductModificationInline,]
+    inlines = [ProductModificationInline, ]  # Встраиваем ProductModificationInline для отображения модификаций
+    # внутри Product.
     list_display = ('title', 'sku', 'category', 'description', 'get_colors', 'get_sizes', 'get_total_stock', 'price',
-                    'created_at', 'thumbnail_image',)
+                    'thumbnail_image', 'created_at')
     search_fields = ('sku',)
-    list_filter = ('sku', 'category', 'colors', 'sizes', )
-
-    def get_total_stock(self, obj):
-        total_stock = sum(mod.stock for mod in obj.modifications.all())
-        return total_stock
-
-    get_total_stock.short_description = 'Сумма остатков'
-
-    def get_colors(self, obj):
-        return ", ".join([color.name for color in obj.colors.all()])
-
-    get_colors.short_description = 'Цвета'
-
-    def get_sizes(self, obj):
-        return ", ".join([size.name for size in obj.sizes.all()])
-
-    get_sizes.short_description = 'Размеры'
-
-    def thumbnail_image(self, obj):
-        images = Image.objects.filter(modification__product=obj)  # Получаем изображения, связанные с этим товаром
-        if images:
-            return format_html('<img src="{}"/>', images[0].thumbnail.url)
-        return format_html('<p>No Image</p>')
-
-    thumbnail_image.allow_tags = True
-    thumbnail_image.short_description = 'Миниатюра изображения'
+    list_filter = ('sku', 'category', 'colors', 'sizes',)
+    readonly_fields = ('created_at', 'updated_at')
 
 
+# Регистрируем модели в административной панели Django.
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductModification, ProductModificationAdmin)
 admin.site.register(Category)
--- deva7km/catalog/models.py
+++ deva7km/catalog/models.py
@@ -1,11 +1,12 @@
-from django.db import models, transaction
-from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.html import format_html
 from django.utils.text import slugify
-from imagekit.models import ImageSpecField, ProcessedImageField
+from imagekit.models import ImageSpecField
 from pilkit.processors import ResizeToFit
 from unidecode import unidecode
 
 
+# Модель товара
 class Product(models.Model):
     title = models.CharField(max_length=200, verbose_name='Наименование')
     category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
@@ -20,17 +21,50 @@ class Product(models.Model):
         ('EUR', 'Евро (EUR)'),
     )
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default='UAH', verbose_name='Валюта')
-    slug = models.SlugField(max_length=200, unique=False, blank=True, verbose_name='Слаг')  # Сделаем slug неуникальным
+    slug = models.SlugField(max_length=200, unique=True, blank=True, verbose_name='Слаг')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    is_active = models.BooleanField(default=True, verbose_name='Включен')
+
+    # Метод для получения общего остатка товара
+    def get_total_stock(self):
+        total_stock = 0
+        for modification in self.modifications.all():
+            total_stock += modification.stock
+        return total_stock
+
+    get_total_stock.short_description = 'Общие остатки'
+
+    # Метод для получения цветов товара в виде строки
+    def get_colors(self):
+        return ", ".join([color.name for color in self.colors.all()])
+
+    get_colors.short_description = 'Цвета товара'
+
+    # Метод для получения размеров товара в виде строки
+    def get_sizes(self):
+        return ", ".join([size.name for size in self.sizes.all()])
+
+    get_sizes.short_description = 'Размеры товара'
+
+    # Метод для отображения миниатюры изображения товара
+    def thumbnail_image(self):
+        images = Image.objects.filter(modification__product=self)
+        if images:
+            return format_html('<img src="{}"/>', images[0].thumbnail.url)
+        return format_html('<p>No Image</p>')
+
+    thumbnail_image.short_description = 'Миниатюра изображения'
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Товар'  # Название модели в единственном числе
-        verbose_name_plural = 'Товары'  # Название модели во множественном числе
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
 
+# Модель модификации товара
 class ProductModification(models.Model):
     product = models.ForeignKey('Product', on_delete=models.CASCADE, verbose_name='Товар', related_name='modifications')
     color = models.ForeignKey('Color', on_delete=models.CASCADE, verbose_name='Цвет')
@@ -40,15 +74,25 @@ class ProductModification(models.Model):
     currency = models.CharField(max_length=3, choices=Product.CURRENCY_CHOICES, default='UAH', verbose_name='Валюта')
     custom_sku = models.CharField(max_length=30, verbose_name='Артикул комплектации', blank=True)
 
+    # Метод для отображения миниатюры изображения модификации товара
+    def thumbnail_image_modification(self):
+        images = Image.objects.filter(modification=self)
+        if images:
+            return format_html('<img src="{}"/>', images[0].thumbnail.url)
+        return format_html('<p>No Image</p>')
+
+    thumbnail_image_modification.short_description = 'Миниатюра изображения'
+
     def __str__(self):
         return f"{self.product} - {self.custom_sku}"
 
     class Meta:
-        verbose_name = 'Модификация товара'  # Название модели в единственном числе
-        verbose_name_plural = 'Модификации товаров'  # Название модели во множественном числе
+        verbose_name = 'Модификация товара'
+        verbose_name_plural = 'Модификации товаров'
         unique_together = ['product', 'color', 'size']
 
 
+# Модель изображения товара
 class Image(models.Model):
     modification = models.ForeignKey('ProductModification', on_delete=models.CASCADE,
                                      verbose_name='Модификация товара', related_name='images')
@@ -57,14 +101,21 @@ class Image(models.Model):
         source='image',
         processors=[ResizeToFit(100, 100)],
         format='JPEG',
-        options={'quality': 60},  # Укажите папку для миниатюр
+        options={'quality': 60},
     )
     large_image = ImageSpecField(
         source='image',
         processors=[ResizeToFit(800, 800)],
         format='JPEG',
-        options={'quality': 90}  # Укажите папку для больших изображений
+        options={'quality': 90}
     )
+
+    # Метод для отображения миниатюры изображения
+    def thumbnail_image(self):
+        return format_html('<img src="{}"/>', self.thumbnail.url)
+
+    thumbnail_image.allow_tags = True
+    thumbnail_image.short_description = 'Миниатюра изображения'
 
     class Meta:
         verbose_name = 'Изображение товара'
@@ -77,13 +128,11 @@ class Image(models.Model):
 # Модель категории товара
 class Category(models.Model):
     name = models.CharField(max_length=200, verbose_name='Наименование категории')
-    slug = models.SlugField(max_length=200, unique=False, blank=True, verbose_name='Слаг')  # Сделаем slug неуникальным
+    slug = models.SlugField(max_length=200, unique=True, blank=True, verbose_name='Слаг')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
     def save(self, *args, **kwargs):
-        # Транслитерируем name
         name_translit = unidecode(self.name)
-        # Создаем slug на основе транслитерированного name
         self.slug = slugify(f"{name_translit}")
         super(Category, self).save(*args, **kwargs)
 
@@ -91,12 +140,13 @@ class Category(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = 'Категория товара'  # Название модели в единственном числе
-        verbose_name_plural = 'Категории товара'  # Название модели во множественном числе
-        ordering = ('name',)  # Сортировка по наименованию категории
-        get_latest_by = 'created_at'  # Последние категории будут отображаться первыми
+        verbose_name = 'Категория товара'
+        verbose_name_plural = 'Категории товара'
+        ordering = ('name',)
+        get_latest_by = 'created_at'
 
 
+# Модель цвета товара
 class Color(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
@@ -104,11 +154,12 @@ class Color(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = 'Цвет'  # Название модели в единственном числе
-        verbose_name_plural = 'Цвета'  # Название модели во множественном числе
+        verbose_name = 'Цвет'
+        verbose_name_plural = 'Цвета'
         ordering = ['name']
 
 
+# Модель размера товара
 class Size(models.Model):
     name = models.CharField(max_length=10, unique=True)
 
@@ -116,6 +167,6 @@ class Size(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = 'Размер'  # Название модели в единственном числе
-        verbose_name_plural = 'Размеры'  # Название модели во множественном числе
+        verbose_name = 'Размер'
+        verbose_name_plural = 'Размеры'
         ordering = ['name']
