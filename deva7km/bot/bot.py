716b22191f722943a50028f113b3c0d72dd61eca
zharuk <deva7km@gmail.com>
2023-11-07 21:12:36 +0200

    работа с ботом

--- deva7km/bot/bot.py
+++ deva7km/bot/bot.py
@@ -8,6 +8,7 @@ from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 from asgiref.sync import sync_to_async
 
+from bot.keyboards.keyboards import inline_keyboard_main_sku
 from catalog.models import Product
 from deva7km.settings import BOT_TOKEN
 
@@ -23,11 +24,8 @@ async def command_start_handler(message: Message) -> None:
     """
     This handler receives messages with `/start` command
     """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    kb = await inline_keyboard_main_sku()
+    await message.answer("Вот ваша клавиатура:", reply_markup=kb)
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
 
--- deva7km/bot/keyboards/keyboards.py
+++ deva7km/bot/keyboards/keyboards.py
@@ -7,17 +7,48 @@ from catalog.models import Product
 async def inline_keyboard_main_sku():
     products = await sync_to_async(list)(Product.objects.all())  # Получаем все товары
 
-    keyboard = InlineKeyboardMarkup(row_width=2)  # Создаем клавиатуру с двумя кнопками в ряду
+    buttons = []
 
+    # Создаем кнопки на основе ключей из data_user > products
     for product in products:
-        button_text = product.sku  # Используем артикул товара в качестве текста кнопки
-        callback_data = f"product_{product.id}"  # Уникальные данные обратного вызова для каждого товара
+        # Преобразуем ключ из байтов в строку
+        sku = product.sku
+        # Пропускаем ключ с названием 'reports'
+        buttons.append(InlineKeyboardButton(text=sku, callback_data=sku + '_main_sku'))
+    # Сортируем кнопки по возрастанию
+    buttons.sort(key=lambda x: int(x.text))
 
-        # Создаем кнопку с текстом и данными обратного вызова
-        button = InlineKeyboardButton(text=button_text, callback_data=callback_data)
+    return InlineKeyboardMarkup(inline_keyboard=[buttons])
 
-        # Добавляем кнопку к клавиатуре
-        keyboard.add(button)
+# async def create_sku_kb(user_id):
+#     # получаем данные из Redis по id пользователя
+#     data_user = await get_data_from_redis(user_id)
+#     # Инициализируем список для кнопок
+#     buttons = []
+#     # Создаем кнопки на основе ключей из data_user > products
+#     for key in data_user['products']:
+#         # Преобразуем ключ из байтов в строку
+#         key_sku = list(key.keys())[0]
+#         # Пропускаем ключ с названием 'reports'
+#         buttons.append(InlineKeyboardButton(text=key_sku, callback_data=key_sku + '_main_sku'))
+#     # Сортируем кнопки по возрастанию
+#     buttons.sort(key=lambda x: int(x.text))
+#
+#     # Задаем количество кнопок в каждом ряду (здесь используется 8)
+#     buttons_per_row = 8
+#     # Разбиваем список кнопок на ряды
+#     rows = [buttons[i:i + buttons_per_row] for i in range(0, len(buttons), buttons_per_row)]
+#
+#     # Добавляем кнопку "Добавить товар" с callback_data='add' в отдельный ряд
+#     buttons_add = InlineKeyboardButton(text='📁 Создать товар', callback_data='add')
+#     rows.append([buttons_add])
+#
+#     # Добавляем кнопку "Вернутся в меню" с callback_data='start' в отдельный ряд
+#     buttons_back = InlineKeyboardButton(text='↩️ Вернуться в меню', callback_data='start')
+#     rows.append([buttons_back])
+#
+#     # Возвращаем объект инлайн-клавиатуры
+#     return InlineKeyboardMarkup(inline_keyboard=rows)
 
-    return keyboard
+
 
