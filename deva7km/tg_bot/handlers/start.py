6100f1f1743719bc68c0760ec3b269140f31f3df
zharuk <deva7km@gmail.com>
2023-11-21 23:35:01 +0200

    убрал локаль

--- deva7km/tg_bot/handlers/start.py
+++ deva7km/tg_bot/handlers/start.py
@@ -23,15 +23,12 @@ async def command_start_handler(message: Message, state: FSMContext) -> None:
     user_last_name = message.from_user.last_name
     # проверка наличия пользователя в бд и если нет то создать
     await get_or_create_telegram_user(user_id, user_name, user_first_name, user_last_name)
-    # Установите русскую локаль
-    locale.setlocale(locale.LC_TIME, 'ru_RU')
     # Получить текущую дату и день недели
     current_date = datetime.datetime.now()
     date_str = current_date.strftime("%d.%m.%Y")
-    day_of_week = current_date.strftime("%A")
     kb = await create_main_menu_kb()
     await message.answer(
-        f"Привет {hbold(message.from_user.full_name)}!\nСегодня {date_str} {hbold(day_of_week)}, что делаем?",
+        f"Привет {hbold(message.from_user.full_name)}!\nСегодня {date_str} , что делаем?",
         reply_markup=kb)
 
 
