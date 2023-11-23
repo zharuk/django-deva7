901f43dac8b19dab375e4d243b181100c2c41d6c
zharuk <deva7km@gmail.com>
2023-11-23 19:01:22 +0200

    run_bot.py

--- deva7km/run_bot.py
+++ deva7km/run_bot.py
@@ -1,10 +1,4 @@
 import os
-import threading
-
-
-# Функция для запуска runserver
-def run_server():
-    os.system("python manage.py runserver")
 
 
 # Функция для запуска runbot
@@ -14,15 +8,8 @@ def run_bot():
 
 if __name__ == "__main__":
     try:
-        # Создание и запуск потока для runserver
-        server_thread = threading.Thread(target=run_server)
-        server_thread.start()
-
         # Запуск runbot в основном потоке
         run_bot()
-
-        # Дождитесь завершения потока runserver
-        server_thread.join()
     except KeyboardInterrupt:
         # При прерывании скрипта пользователем выполните необходимые действия
         print("run_both.py: Скрипт был прерван пользователем")
