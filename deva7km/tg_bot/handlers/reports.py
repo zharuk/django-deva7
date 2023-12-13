ed5b19a684e5e44885d52194a74a50c6dbbb3d42
zharuk <deva7km@gmail.com>
2023-12-13 19:59:47 +0200

    правка4

--- deva7km/tg_bot/handlers/reports.py
+++ deva7km/tg_bot/handlers/reports.py
@@ -2,10 +2,8 @@ from aiogram import Router, Bot
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
-from django.utils import asyncio
-
 from deva7km.settings import BOT_TOKEN
-from tg_bot.keyboards.keyboards import create_report_kb, create_inline_kb_cancel
+from tg_bot.keyboards.keyboards import create_report_kb
 from tg_bot.services.reports import generate_sales_report_by_day, generate_sales_report_by_yesterday, \
     generate_sales_report_by_week, generate_sales_report_by_month, generate_sales_report_by_year, get_total_stock
 from tg_bot.services.users import admin_access_control_decorator
@@ -39,7 +37,16 @@ async def process_callback_query_sell(callback: CallbackQuery, state: FSMContext
 async def process_callback_query_sell(callback: CallbackQuery):
     report = await generate_sales_report_by_day()
     kb = await create_report_kb()
-    await callback.message.answer(report, reply_markup=kb)
+    max_length = 4096  # Максимальная длина сообщения Telegram
+
+    # Разделение текста на части
+    parts = [report[i:i + max_length] for i in range(0, len(report), max_length)]
+
+    # Отправка каждой части поочередно
+    for part in parts:
+        await callback.message.answer(part)
+
+    await callback.message.answer('\nОтчет закончен', reply_markup=kb)
     await callback.answer()
 
 
@@ -49,7 +56,16 @@ async def process_callback_query_sell(callback: CallbackQuery):
 async def process_callback_query_sell(callback: CallbackQuery):
     report = await generate_sales_report_by_yesterday()
     kb = await create_report_kb()
-    await callback.message.answer(report, reply_markup=kb)
+    max_length = 4096  # Максимальная длина сообщения Telegram
+
+    # Разделение текста на части
+    parts = [report[i:i + max_length] for i in range(0, len(report), max_length)]
+
+    # Отправка каждой части поочередно
+    for part in parts:
+        await callback.message.answer(part)
+
+    await callback.message.answer('\nОтчет закончен', reply_markup=kb)
     await callback.answer()
 
 
@@ -59,7 +75,16 @@ async def process_callback_query_sell(callback: CallbackQuery):
 async def process_callback_query_sell(callback: CallbackQuery):
     report = await generate_sales_report_by_week()
     kb = await create_report_kb()
-    await callback.message.answer(report, reply_markup=kb)
+    max_length = 4096  # Максимальная длина сообщения Telegram
+
+    # Разделение текста на части
+    parts = [report[i:i + max_length] for i in range(0, len(report), max_length)]
+
+    # Отправка каждой части поочередно
+    for part in parts:
+        await callback.message.answer(part)
+
+    await callback.message.answer('\nОтчет закончен', reply_markup=kb)
     await callback.answer()
 
 
@@ -69,7 +94,16 @@ async def process_callback_query_sell(callback: CallbackQuery):
 async def process_callback_query_sell(callback: CallbackQuery):
     report = await generate_sales_report_by_month()
     kb = await create_report_kb()
-    await callback.message.answer(report, reply_markup=kb)
+    max_length = 4096  # Максимальная длина сообщения Telegram
+
+    # Разделение текста на части
+    parts = [report[i:i + max_length] for i in range(0, len(report), max_length)]
+
+    # Отправка каждой части поочередно
+    for part in parts:
+        await callback.message.answer(part)
+
+    await callback.message.answer('\nОтчет закончен', reply_markup=kb)
     await callback.answer()
 
 
@@ -79,7 +113,16 @@ async def process_callback_query_sell(callback: CallbackQuery):
 async def process_callback_query_sell(callback: CallbackQuery):
     report = await generate_sales_report_by_year()
     kb = await create_report_kb()
-    await callback.message.answer(report, reply_markup=kb)
+    max_length = 4096  # Максимальная длина сообщения Telegram
+
+    # Разделение текста на части
+    parts = [report[i:i + max_length] for i in range(0, len(report), max_length)]
+
+    # Отправка каждой части поочередно
+    for part in parts:
+        await callback.message.answer(part)
+
+    await callback.message.answer('\nОтчет закончен', reply_markup=kb)
     await callback.answer()
 
 
