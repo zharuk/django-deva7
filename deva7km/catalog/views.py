27cab603146779853de1f46533678c5b43d62adc
zharuk <deva7km@gmail.com>
2023-12-17 16:54:48 +0200

    отредактировал сортировку

--- deva7km/catalog/views.py
+++ deva7km/catalog/views.py
@@ -3,11 +3,10 @@ from django.views import View
 from catalog.models import Image, Category, Product
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Category
 
 
 def home(request):
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(product_count=Count('product')).order_by('-product_count')
     latest_products = Product.objects.order_by('-created_at')[:6]
     return render(request, 'home.html', {'categories': categories, 'latest_products': latest_products})
 
@@ -17,7 +16,7 @@ def category_detail(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
 
     # Получение всех товаров в данной категории и упорядочивание их по полю title
-    products_list = category.product_set.order_by('title')
+    products_list = category.product_set.order_by('-created_at')
 
     # Количество товаров, которое вы хотите отобразить на каждой странице
     items_per_page = 9
@@ -39,7 +38,7 @@ def category_detail(request, category_slug):
         products = paginator.page(paginator.num_pages)
 
     # Получение всех категорий для формирования меню
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(product_count=Count('product')).order_by('-product_count')
 
     # Передача данных в контекст для использования в шаблоне
     context = {'category': category, 'products': products, 'categories': categories}
@@ -64,7 +63,7 @@ def product_detail(request, category_slug, product_slug):
             unique_color_images[color['color__name']] = images
 
     # Получение всех категорий (или нужные данные для формирования меню)
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(product_count=Count('product')).order_by('-product_count')
 
     return render(request, 'product_detail.html', {'product': product, 'categories': categories,
                                                    'unique_color_images': unique_color_images})
@@ -73,26 +72,26 @@ def product_detail(request, category_slug, product_slug):
 class AllProductsView(View):
     def get(self, request):
         # Логика для отображения всех товаров
-        products = Product.objects.all()  # Пример, ты можешь адаптировать под свою модель
-        categories = Category.objects.all()
+        products = Product.objects.all().order_by('-created_at')
+        categories = Category.objects.annotate(product_count=Count('product')).order_by('-product_count')
         return render(request, 'all_products.html', {'products': products, 'categories': categories})
 
 
 def about_page(request):
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(product_count=Count('product')).order_by('-product_count')
     return render(request, 'about_page.html', {'categories': categories})
 
 
 def contacts_page(request):
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(product_count=Count('product')).order_by('-product_count')
     return render(request, 'contacts_page.html', {'categories': categories})
 
 
 def delivery_page(request):
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(product_count=Count('product')).order_by('-product_count')
     return render(request, 'delivery_page.html', {'categories': categories})
 
 
 def payment_page(request):
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(product_count=Count('product')).order_by('-product_count')
     return render(request, 'payment_page.html', {'categories': categories})
