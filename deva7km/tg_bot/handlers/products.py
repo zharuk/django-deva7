9c44fd3022982e3f74c791bc9015e4c5884a184e
zharuk <deva7km@gmail.com>
2024-01-08 11:54:55 +0200

    поправил списание товара

--- deva7km/tg_bot/handlers/products.py
+++ deva7km/tg_bot/handlers/products.py
@@ -5,6 +5,7 @@ from deva7km.settings import BOT_TOKEN
 from tg_bot.keyboards.keyboards import create_inline_kb_main_sku, create_inline_kb_return
 from tg_bot.services.products import get_modifications_info, get_first_image_for_product
 from tg_bot.services.users import admin_access_control_decorator
+import re
 
 router: Router = Router()
 bot: Bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
@@ -31,22 +32,32 @@ async def process_callback_query_products(callback: CallbackQuery):
 @router.callback_query(lambda callback: callback.data.startswith('prev_'))
 @admin_access_control_decorator(access='seller')
 async def process_callback_query_prev(callback: CallbackQuery):
-    _, callback_name, page = callback.data.split('_')
-    page = int(page) - 1 if int(page) > 1 else 1
-    kb = await create_inline_kb_main_sku(callback_name, page)
-    await callback.message.edit_reply_markup(reply_markup=kb)
-    await callback.answer()
+    match = re.match(r'prev_(\w+)_([0-9]+)', callback.data)
+    if match:
+        callback_name, page = match.groups()
+        page = int(page) - 1 if int(page) > 1 else 1
+        kb = await create_inline_kb_main_sku(callback_name, page)
+        await callback.message.edit_reply_markup(reply_markup=kb)
+        await callback.answer()
+    else:
+        # Обработка ситуации, когда формат не соответствует ожидаемому
+        print(f"Некорректный формат данных в callback.data: {callback.data}")
 
 
 # Хендлер для кнопки Вперед
 @router.callback_query(lambda callback: callback.data.startswith('next_'))
 @admin_access_control_decorator(access='seller')
 async def process_callback_query_next(callback: CallbackQuery):
-    _, callback_name, page = callback.data.split('_')
-    page = int(page) + 1
-    kb = await create_inline_kb_main_sku(callback_name, page)
-    await callback.message.edit_reply_markup(reply_markup=kb)
-    await callback.answer()
+    match = re.match(r'next_(\w+)_([0-9]+)', callback.data)
+    if match:
+        callback_name, page = match.groups()
+        page = int(page) + 1
+        kb = await create_inline_kb_main_sku(callback_name, page)
+        await callback.message.edit_reply_markup(reply_markup=kb)
+        await callback.answer()
+    else:
+        # Обработка ситуации, когда формат не соответствует ожидаемому
+        print(f"Некорректный формат данных в callback.data: {callback.data}")
 
 
 # обработчик который бы отлавливал callback_query=sku для products
