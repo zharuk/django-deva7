7b7c5755a56f5ed353202baa7c4ee83b4a19b711
zharuk <deva7km@gmail.com>
2024-03-21 18:01:27 +0200

    правка по языкам 20

--- deva7km/deva7km/settings.py
+++ deva7km/deva7km/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'uk'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Europe/Kiev'
 
@@ -117,8 +117,8 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LANGUAGES = [
-    ("uk", _("Ukrainian")),
     ("ru", _("Russian")),
+    ("uk", _("Ukrainian")),
 ]
 
 LOCALE_PATHS = (
