31fb65fe76e1d7168899b9b84d804468715fc630
zharuk <deva7km@gmail.com>
2024-03-21 17:44:46 +0200

    правка по языкам 15

--- deva7km/catalog/middlewares.py
+++ deva7km/catalog/middlewares.py
@@ -13,16 +13,8 @@ class FrontendLanguageMiddleware:
             activate(settings.LANGUAGE_CODE)
         else:
             # В противном случае активируем язык, выбранный пользователем на фронтенде
-            if 'language_selected' not in request.session:
-                # Если пользователь новый и не выбрал язык, устанавливаем украинский язык по умолчанию
-                activate('uk')
-                # Устанавливаем флаг выбора языка в сессии пользователя
-                request.session['language_selected'] = True
-            else:
-                # Если язык уже был выбран пользователем, активируем его
-                activate(request.LANGUAGE_CODE)
+            activate(request.LANGUAGE_CODE)
 
         response = self.get_response(request)
         return response
-
 
