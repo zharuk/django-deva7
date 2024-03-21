13438695b6ced4dfa0f3f4da930be35dc3c2f731
zharuk <deva7km@gmail.com>
2024-03-21 18:14:34 +0200

    правка по языкам 23

--- deva7km/deva7km/settings.py
+++ deva7km/deva7km/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'uk'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Europe/Kiev'
 
