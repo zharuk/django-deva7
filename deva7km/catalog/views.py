0f7ed64067267d825b92710658ce3f7f31c6976d
zharuk <deva7km@gmail.com>
2024-03-30 18:34:23 +0200

    оформление заказа

--- deva7km/catalog/views.py
+++ deva7km/catalog/views.py
@@ -261,6 +261,7 @@ def cart_view(request):
     categories = Category.objects.annotate(product_count=Count('product')).order_by('-product_count')
 
     cart_items_json = json.dumps([{'modification': {'custom_sku': item['modification'].custom_sku}, 'quantity': item['quantity']} for item in cart_items])
+    print(cart_items_json)
 
     return render(request, 'cart.html',
                   {'categories': categories, 'cart_items': cart_items, 'cart_total_price': cart_total_price,
