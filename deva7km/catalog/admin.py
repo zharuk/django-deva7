54697e4363d6de7b55a40b2720d8d666d9648e9b
zharuk <deva7km@gmail.com>
2024-06-15 15:52:01 +0300

    правки предзаказов 4

--- deva7km/catalog/admin.py
+++ deva7km/catalog/admin.py
@@ -225,7 +225,7 @@ class OrderAdmin(admin.ModelAdmin):
 class PreOrderAdmin(admin.ModelAdmin):
     list_display = (
         'full_name', 'id', 'text', 'drop', 'receipt_issued', 'ttn', 'shipped_to_customer', 'created_at', 'updated_at',)
-    list_filter = ('shipped_to_customer')
+    list_filter = 'shipped_to_customer'
     search_fields = ('full_name', 'text', 'ttn')
 
 
