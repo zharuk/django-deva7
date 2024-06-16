6505710b9f4208c7f5cb08d8b5201f0f0f434dcd
zharuk <deva7km@gmail.com>
2024-06-16 15:39:35 +0300

    трекинг правка 3

---
+++ deva7km/catalog/management/commands/update_tracking_gunicorn.py
@@ -0,0 +1,11 @@
+# deva7km/catalog/management/commands/update_tracking_gunicorn.py
+from django.core.management.base import BaseCommand
+from catalog.management.commands import update_tracking_status
+
+
+class Command(BaseCommand):
+    help = 'Updates tracking statuses using Gunicorn'
+
+    def handle(self, *args, **options):
+        update_tracking_status.update_tracking_status()
+        self.stdout.write(self.style.SUCCESS('Successfully updated tracking statuses'))
