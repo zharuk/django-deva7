cd9ca26f02942d8112bd18e996eb9e6605c90dab
zharuk <deva7km@gmail.com>
2024-06-17 20:22:17 +0300

    изменил обновление статусов по кнопке 3

--- deva7km/catalog/management/commands/update_tracking_status.py
+++ deva7km/catalog/management/commands/update_tracking_status.py
@@ -3,10 +3,10 @@ import time
 import logging
 from django.core.cache import cache
 from django.conf import settings
+from django.utils import timezone
 
 # Логирование
 logger_tracking = logging.getLogger('tracking')
-
 
 def update_tracking_status(preorder):
     api_key = getattr(settings, 'NOVA_POSHTA_API_KEY', None)
@@ -17,13 +17,13 @@ def update_tracking_status(preorder):
 
     # Попробуем получить статус из кэша
     cache_key = f"tracking_status_{preorder.ttn}"
-    status = cache.get(cache_key)
+    status_with_time = cache.get(cache_key)
 
-    if status:
+    if status_with_time:
         # Если статус есть в кэше, обновляем и сохраняем предзаказ
-        preorder.status = status
+        preorder.status = status_with_time
         preorder.save()
-        logger_tracking.info(f"Обновлен статус из кэша для заказа {preorder.id}. Новый статус: {status}")
+        logger_tracking.info(f"Обновлен статус из кэша для заказа {preorder.id}. Новый статус: {status_with_time}")
         return
 
     # Если статуса нет в кэше, делаем запрос к API
@@ -48,14 +48,23 @@ def update_tracking_status(preorder):
 
     if data['success']:
         status = data['data'][0]['Status']
-        preorder.status = status
+        current_time = timezone.now().strftime('%d-%m-%Y %H:%M:%S')
+
+        # Не обновляем статусы, содержащие "Відмова від отримання" и "Відправлення отримано"
+        if "Відмова від отримання" in status or "Відправлення отримано" in status:
+            logger_tracking.info(f"Статус заказа {preorder.id} не был обновлен, так как он содержит '{status}'")
+            return
+
+        # Добавляем время обновления к статусу
+        status_with_time = f"{status} (обновлено: {current_time})"
+        preorder.status = status_with_time
         preorder.save()
 
         # Кэшируем статус на 10 минут
-        cache.set(cache_key, status, timeout=600)
+        cache.set(cache_key, status_with_time, timeout=600)
 
         # Логирование успешного обновления статуса трекинга
-        logger_tracking.info(f"Обновлен статус для заказа {preorder.id}. Новый статус: {status}")
+        logger_tracking.info(f"Обновлен статус для заказа {preorder.id}. Новый статус: {status_with_time}")
     else:
         # Логирование ошибок при запросе статуса трекинга
         logger_tracking.error(f"Ошибка при обновлении статуса для заказа {preorder.id}. Ответ API: {data}")
@@ -63,4 +72,3 @@ def update_tracking_status(preorder):
     # Логирование всех запросов и ответов API
     logger_tracking.debug(f"API запрос для заказа {preorder.id}: {payload}")
     logger_tracking.debug(f"API ответ для заказа {preorder.id}: {data}")
-
