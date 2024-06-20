bc8ea3be774d76b0ea9174117c7cc7a92617c541
zharuk <deva7km@gmail.com>
2024-06-20 20:16:24 +0300

    правка обновления заказов 2

--- deva7km/catalog/management/commands/update_tracking_status.py
+++ deva7km/catalog/management/commands/update_tracking_status.py
@@ -8,7 +8,10 @@ import logging
 # Настройка логирования
 logger_tracking = logging.getLogger('tracking')
 
-async def get_tracking_status_from_api_nova_poshta(ttn, api_key):
+# Настройка логирования
+logger_tracking = logging.getLogger('tracking')
+
+async def get_tracking_status_from_api_nova_poshta(ttns, api_key):
     """Асинхронный запрос статуса посылки по API Nova Poshta."""
     url = 'https://api.novaposhta.ua/v2.0/json/'
     payload = {
@@ -16,11 +19,7 @@ async def get_tracking_status_from_api_nova_poshta(ttn, api_key):
         "modelName": "TrackingDocument",
         "calledMethod": "getStatusDocuments",
         "methodProperties": {
-            "Documents": [
-                {
-                    "DocumentNumber": ttn
-                }
-            ]
+            "Documents": [{"DocumentNumber": ttn} for ttn in ttns]
         }
     }
 
@@ -29,84 +28,59 @@ async def get_tracking_status_from_api_nova_poshta(ttn, api_key):
         "Accept": "application/json"
     }
 
-    logger_tracking.info(f"Отправка запроса для TTN {ttn}: {payload}")
+    logger_tracking.info(f"Отправка запроса для TTN {ttns}: {payload}")
 
     async with aiohttp.ClientSession() as session:
         async with session.post(url, json=payload, headers=headers) as response:
+            response_text = await response.text()
             if response.status != 200:
                 logger_tracking.error(
-                    f"Неудачный запрос к API Nova Poshta для заказа с TTN {ttn}. HTTP статус: {response.status}")
+                    f"Неудачный запрос к API Nova Poshta для TTNs {ttns}. HTTP статус: {response.status}. Ответ: {response_text}")
                 return None
 
             content_type = response.headers.get('Content-Type', '')
 
             if 'application/json' in content_type:
                 data = await response.json()
-                logger_tracking.info(f"Получен ответ для TTN {ttn}: {data}")
+                logger_tracking.info(f"Получен ответ для TTNs {ttns}: {data}")
                 return data
             else:
-                response_text = await response.text()
                 logger_tracking.error(
-                    f"Неверный тип ответа от API Nova Poshta для заказа с TTN {ttn}. Получен тип: {content_type}. Ответ: {response_text}")
+                    f"Неверный тип ответа от API Nova Poshta для TTNs {ttns}. Получен тип: {content_type}. Ответ: {response_text}")
                 return None
 
-async def update_tracking_status(preorder):
+async def update_tracking_status(preorders):
+    # Проверка и преобразование в список, если передан один объект
+    if not isinstance(preorders, list):
+        preorders = [preorders]
+
     api_key = getattr(settings, 'NOVA_POSHTA_API_KEY', None)
-    carrier = get_carrier(preorder.ttn)
+    carriers = {preorder.ttn: get_carrier(preorder.ttn) for preorder in preorders}
+    ttns_nova_poshta = [ttn for ttn, carrier in carriers.items() if carrier == 'NovaPoshta']
 
     try:
-        logger_tracking.info(f"Начало обновления статуса для заказа {preorder.id} с TTN {preorder.ttn}")
-
-        if carrier == 'NovaPoshta':
-            if not api_key:
-                logger_tracking.error("NOVA_POSHTA_API_KEY не установлен.")
-                return
-
-            # Получаем статус из API Nova Poshta
-            data = await get_tracking_status_from_api_nova_poshta(preorder.ttn, api_key)
-
-        elif carrier == 'UkrPoshta':
-            # Получаем статус из API UkrPoshta
-            data = await get_tracking_status_from_api_ukrposhta(preorder.ttn)
-
-        else:
-            logger_tracking.error(f"Неизвестный перевозчик для заказа {preorder.id} с TTN {preorder.ttn}.")
+        if not api_key and ttns_nova_poshta:
+            logger_tracking.error("NOVA_POSHTA_API_KEY не установлен.")
             return
 
-        if not data:
-            logger_tracking.error(f"Не удалось получить данные от API для заказа {preorder.id}.")
-            return
-
-        if not data.get('success'):
-            logger_tracking.error(f"Ошибка при обновлении статуса для заказа {preorder.id}. Ответ API: {data}")
-            return
-
-        if not data.get('data'):
-            logger_tracking.error(f"Ответ API не содержит данных для заказа {preorder.id}. Ответ API: {data}")
-            return
-
-        status = data['data'][0].get('Status')
-        if not status:
-            logger_tracking.error(f"Ответ API не содержит статус для заказа {preorder.id}. Ответ API: {data}")
-            return
-
-        current_time = timezone.localtime().strftime('%d.%m.%Y %H:%M:%S')
-        current_admin_status = preorder.status
-
-        # Обновляем статус, если он пустой или не содержит "Відправлення отримано"
-        if not current_admin_status or "Відправлення отримано" not in status:
-            status_with_time = f"{status} (обновлено: {current_time})"
-            preorder.status = status_with_time
-            await sync_to_async(preorder.save)()
-
-            logger_tracking.info(f"Обновлен статус для заказа {preorder.id}. Новый статус: {status_with_time}")
-
-        else:
-            logger_tracking.info(
-                f"Статус заказа {preorder.id} не был обновлен, так как текущий статус содержит '{status}'")
+        # Пакетная обработка для Nova Poshta
+        if ttns_nova_poshta:
+            data = await get_tracking_status_from_api_nova_poshta(ttns_nova_poshta, api_key)
+            if data and data.get('success'):
+                for document in data.get('data', []):
+                    ttn = document.get('Number')
+                    status = document.get('Status')
+                    if ttn and status:
+                        preorder = next((p for p in preorders if p.ttn == ttn), None)
+                        if preorder:
+                            current_time = timezone.localtime().strftime('%d.%m.%Y %H:%M:%S')
+                            status_with_time = f"{status} (обновлено: {current_time})"
+                            preorder.status = status_with_time
+                            await sync_to_async(preorder.save)()
+                            logger_tracking.info(f"Обновлен статус для заказа {preorder.id}. Новый статус: {status_with_time}")
 
     except Exception as e:
-        logger_tracking.error(f"Произошла ошибка при обновлении статуса для заказа {preorder.id}: {e}")
+        logger_tracking.error(f"Произошла ошибка при обновлении статуса: {e}")
 
 def get_carrier(ttn):
     """Определение службы доставки по номеру ТТН."""
@@ -129,3 +103,5 @@ async def get_tracking_status_from_api_ukrposhta(ttn):
         ]
     }
     return fake_response
+
+
