6d8bc74cce07f60a79e8b2cf11a4bb1d54c7be4e
zharuk <deva7km@gmail.com>
2024-07-11 11:57:10 +0300

    Поправил asgi

--- deva7km/deva7km/asgi.py
+++ deva7km/deva7km/asgi.py
@@ -1,10 +1,13 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import deva7km.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deva7km.settings')
+
+django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
@@ -14,3 +17,8 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+
+# Для теста запустите этот файл отдельно
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("deva7km.asgi:application", host="0.0.0.0", port=8000, log_level="info")
