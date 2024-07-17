17b2d28934ecb05e16c0b3567ae138f0c592e012
zharuk <deva7km@gmail.com>
2024-07-17 20:30:15 +0300

    правки + кнопка обновления статуса ттн 2

--- deva7km/catalog/novaposhta.py
+++ deva7km/catalog/novaposhta.py
@@ -35,11 +35,7 @@ async def update_tracking_status():
     ).delete)()
     logger.info("Old preorders or received ones deleted.")
 
-    # Получаем все TTN из базы данных, которые были обновлены не менее 30 минут назад
-    thirty_minutes_ago = now - timezone.timedelta(minutes=30)
-    # all_ttns = await sync_to_async(list)(
-    #     PreOrder.objects.filter(updated_at__lt=thirty_minutes_ago).values_list('ttn', flat=True)
-    # )
+    # Получаем все TTN из базы данных
     all_ttns = await sync_to_async(list)(
         PreOrder.objects.values_list('ttn', flat=True)
     )
@@ -53,38 +49,43 @@ async def update_tracking_status():
         logger.info("No valid TTNs found for updating status.")
         return
 
-    status_response = get_tracking_status(cleaned_ttns)
-    logger.info(f"Status response from Nova Poshta: {status_response}")
+    # Разбиваем список TTN на порции по 100 элементов
+    chunk_size = 100
+    chunks = [cleaned_ttns[i:i + chunk_size] for i in range(0, len(cleaned_ttns), chunk_size)]
 
-    if not status_response.get('success'):
-        logger.error("Failed to get status from Nova Poshta.")
-        return
+    for chunk in chunks:
+        status_response = get_tracking_status(chunk)
+        logger.info(f"Status response from Nova Poshta: {status_response}")
 
-    status_data_list = status_response.get('data', [])
-    logger.info(f"Status data list: {status_data_list}")
-
-    for status_data in status_data_list:
-        cleaned_ttn_number = status_data['Number']
-        status = status_data['Status']
-
-        original_ttn_number = ttn_mapping.get(cleaned_ttn_number)
-        if not original_ttn_number:
-            logger.warning(f"No original TTN found for cleaned TTN {cleaned_ttn_number}")
+        if not status_response.get('success'):
+            logger.error("Failed to get status from Nova Poshta.")
             continue
 
-        try:
-            preorders = await sync_to_async(list)(PreOrder.objects.filter(ttn=original_ttn_number))
-            if not preorders:
-                logger.warning(f"PreOrder with TTN {original_ttn_number} does not exist.")
+        status_data_list = status_response.get('data', [])
+        logger.info(f"Status data list: {status_data_list}")
+
+        for status_data in status_data_list:
+            cleaned_ttn_number = status_data['Number']
+            status = status_data['Status']
+
+            original_ttn_number = ttn_mapping.get(cleaned_ttn_number)
+            if not original_ttn_number:
+                logger.warning(f"No original TTN found for cleaned TTN {cleaned_ttn_number}")
                 continue
-        except Exception as e:
-            logger.error(f"Error retrieving PreOrder with TTN {original_ttn_number}: {e}")
-            continue
 
-        for preorder in preorders:
-            preorder.status = status
-            preorder.tracking_status = status
-            preorder.updated_at = now
+            try:
+                preorders = await sync_to_async(list)(PreOrder.objects.filter(ttn=original_ttn_number))
+                if not preorders:
+                    logger.warning(f"PreOrder with TTN {original_ttn_number} does not exist.")
+                    continue
+            except Exception as e:
+                logger.error(f"Error retrieving PreOrder with TTN {original_ttn_number}: {e}")
+                continue
 
-            await sync_to_async(preorder.save)()
-            logger.info(f"PreOrder with TTN {original_ttn_number} updated successfully.")
+            for preorder in preorders:
+                preorder.status = status
+                preorder.tracking_status = status
+                preorder.updated_at = now
+
+                await sync_to_async(preorder.save)()
+                logger.info(f"PreOrder with TTN {original_ttn_number} updated successfully.")
