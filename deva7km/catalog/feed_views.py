b090c4adb588cac2fab7039044ea02a80f3bafc7
sine <deva7km@gmail.com>
2025-06-29 15:47:13 +0300

    Правка фида фб 2

--- deva7km/catalog/feed_views.py
+++ deva7km/catalog/feed_views.py
@@ -1,19 +1,45 @@
+from catalog.models import Product, ProductModification, Image, Category
+from django.views import View
 from django.http import HttpResponse
 from django.template import loader
-from django.views import View
-from catalog.models import Product, ProductModification, Image, Category
+from django.utils.translation import activate
+from django.conf import settings
 
 
 class FacebookFeedView(View):
     @staticmethod
     def get(request, *args, **kwargs):
+        # Получаем язык из параметра URL, по умолчанию украинский
+        language = request.GET.get('lang', 'uk')
+
+        # Проверяем, что язык поддерживается
+        available_languages = [lang[0] for lang in settings.LANGUAGES]
+        if language not in available_languages:
+            language = 'uk'  # fallback на украинский
+
+        # Активируем нужный язык
+        activate(language)
+
         products = Product.objects.filter(is_active=True)
         modifications = ProductModification.objects.all()
         images = Image.objects.all()
-        context = {'products': products, 'modifications': modifications, 'images': images, 'request': request}
+
+        # Создаем копию request с нужным языком
+        request_copy = request
+        request_copy.LANGUAGE_CODE = language
+
+        context = {
+            'products': products,
+            'modifications': modifications,
+            'images': images,
+            'request': request_copy,
+            'language': language  # добавляем язык в контекст для шаблона
+        }
+
         template = loader.get_template('fb_feed.xml')
         xml_content = template.render(context)
         response = HttpResponse(xml_content, content_type='application/xml')
+
         return response
 
 
